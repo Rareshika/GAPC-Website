ab7f46ade848b483ceb8171a832ac371c1c96bb3
masecla22 <supermasecla22@gmail.com>
2025-03-31 11:51:27 +0200

    mea

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,32 @@ export default function RootLayout({
     <html lang="en">
       <Head>
         <link rel="icon" type="image/png" href="/favicon.ico" />
+
+        <meta name="og:title" content="GAPC 2025" />
+        <meta name="og:type" content="programming_competition" />
+
+        <meta name="og:url" content="https://gapc.svcover.nl/" />
+        <meta
+          name="og:image"
+          content="https://filemanager.svcover.nl/images/resize?f=uploads%2Fprogramming_committee%2FGAPC%2FGAPC+Cover+Banner.png&w=1000"
+        />
+
+        <meta name="og:site_name" content="GAPC 2025 Name" />
+        <meta
+          name="og:description"
+          content="The programming competition organized in Groningen!"
+        />
+
+        <meta name="application-name" content="gapc2025website" />
+        <meta name="og:email" content="programming_committee@svcover.nl" />
+
+        <meta name="og:latitude" content="53.2403955" />
+        <meta name="og:longitude" content="6.5338483" />
+        <meta name="og:street-address" content="Zernikelaan" />
+        <meta name="og:locality" content="Groningen" />
+        <meta name="og:region" content="Groningen" />
+        <meta name="og:postal-code" content="9747AG" />
+        <meta name="og:country-name" content="The Netherlands" />
       </Head>
       <body className={inter.className}>
         <NavBar />
